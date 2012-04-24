a665ad01861a6feab150c8081d35e3351228aec0
rabid-fish <Dan@djuliano.(none)>
2012-04-24 17:53:07 -0500

    Removing binary files

--- cijug-webservice/target/generated-sources/cxf/https/github_com/rabid_fish/math/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "https://github.com/rabid-fish/math", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package https.github_com.rabid_fish.math;
